55d637cd5e01ab0dd84e2e8292b484f7037f8809
Triin <triin.varblane@gmail.com>
2024-12-17 16:34:22 +0200

    add week 4 code

---
+++ RandomNumber/Program.cs
@@ -0,0 +1,22 @@
+﻿Random rnd = new Random();
+
+//int myRandomNum = rnd.Next(0, 11); //ehk 0-10
+//Console.WriteLine($"My random number is: {myRandomNum}");
+
+int myRandomNum;
+
+//for(int i = 0; i < 3; i++)
+//{
+//    myRandomNum = rnd.Next(0, 11);
+//    Console.WriteLine($"My random number is: {myRandomNum}");
+//}
+
+int randomSum = 0;
+
+for (int i = 0; i < 3; i++)
+{
+    myRandomNum = rnd.Next(0, 11);
+    randomSum = randomSum + myRandomNum;
+    Console.WriteLine($"My random number is: {myRandomNum}");
+}
+Console.WriteLine($"random sum total: {randomSum}");
